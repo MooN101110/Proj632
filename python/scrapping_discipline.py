cc95d7f276b0df28c03483056ad8c68d0257d3da
Louna Camas <lounacamas@gmail.com>
2024-03-09 16:32:55 +0100

    Create scrapping_discipline.py

---
+++ python/scrapping_discipline.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+import chromedriver_autoinstaller
+import csv
+
+url='https://www.polytech.univ-smb.fr/intranet/accueil.html'
+
+#Ouverture de la page web
+chromedriver_autoinstaller.install()
+
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+html = driver.page_source
+
+soup= bs(html, 'html.parser')
+
+#Connexion
+
+username=driver.find_element(By.ID,'user')
+password=driver.find_element(By.NAME,'pass')
+
+#Récuperation information 
+file=open("login.txt", "r")
+lines=file.readlines()
+
+username.send_keys(lines[0])
+password.send_keys(lines[1])
+
+try:
+    driver.find_element(By.NAME,"submit").submit()
+except NoSuchElementException as e:
+    pass
+
+#Aller à la page programme
+soup =bs(driver.page_source, "lxml")
+lien=soup.find(title="Accéder aux programmes")["href"]
+driver.get("https://www.polytech.univ-smb.fr"+lien)
+
+soup= bs(driver.page_source, 'html.parser')
+
+#Récupération des disciplines
+recup_disciplines=soup.find('select',id="discipline")
+disciplines=[]
+for elt in (recup_disciplines):
+    if elt!='\n':  #enlever les \n collés aux noms des disciplines
+        if elt.get_text()!='---':  #e,elevr les premier elt qui n'est pas une discipline (ne peut pas être fait avec un if and)
+            disciplines.append([elt.get_text()])
+
+# Sauvegarde des données
+with open("disciplines.csv", "wt+", newline="") as f:
+    writer = csv.writer(f,delimiter=',')
+    for row in disciplines:
+        writer.writerow(row)
+        
+driver.close()
