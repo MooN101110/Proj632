ce40f951643ecc17c8df050c3b274d4edf5af3db
Louna Camas <lounacamas@gmail.com>
2024-03-12 09:13:54 +0100

    Enregistrement disciplines bd

--- python/scrapping_discipline.py
+++ python/scrapping_discipline.py
@@ -6,6 +6,8 @@ import time
 import chromedriver_autoinstaller
 import csv
 
+import lien_db
+
 url='https://www.polytech.univ-smb.fr/intranet/accueil.html'
 
 #Ouverture de la page web
@@ -20,12 +22,11 @@ html = driver.page_source
 soup= bs(html, 'html.parser')
 
 #Connexion
-
 username=driver.find_element(By.ID,'user')
 password=driver.find_element(By.NAME,'pass')
 
 #Récuperation information 
-file=open("login.txt", "r")
+file=open("logs.txt", "r")
 lines=file.readlines()
 
 username.send_keys(lines[0])
@@ -49,12 +50,23 @@ disciplines=[]
 for elt in (recup_disciplines):
     if elt!='\n':  #enlever les \n collés aux noms des disciplines
         if elt.get_text()!='---':  #e,elevr les premier elt qui n'est pas une discipline (ne peut pas être fait avec un if and)
-            disciplines.append([elt.get_text()])
-
-# Sauvegarde des données
-with open("disciplines.csv", "wt+", newline="") as f:
-    writer = csv.writer(f,delimiter=',')
-    for row in disciplines:
-        writer.writerow(row)
-        
+            disciplines.append(elt.get_text())
+            #Mettre cette ligne pour csv
+            #disciplines.append([elt.get_text()])
+            
 driver.close()
+
+# # Sauvegarde des données en csv
+# with open("disciplines.csv", "wt+", newline="") as f:
+#     writer = csv.writer(f,delimiter=',')
+#     for row in disciplines:
+#         writer.writerow(row)
+        
+#Sauvegarde des données dans la bd
+bd=lien_db.get_db("logs_db.txt")
+for elt in (disciplines):
+    query= f"INSERT INTO INFO_discipline (nom) VALUES ('{elt}')"
+    lien_db.execute_query(bd,query)
+
+print(lien_db.get_data(bd,"INFO_discipline"))
+lien_db.close_db(bd)
