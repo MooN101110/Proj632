aae87df1e74c835450f1aac874890f4cebd04fea
Louna Camas <lounacamas@gmail.com>
2024-04-02 15:05:22 +0200

    Mise en forme fichier

---
+++ html_css/page_inscription.php
@@ -0,0 +1,20 @@
+
+<form method='post' >
+  <input type='text' name='nom' placeholder="Nom" required><br/>
+  <input type='text' name='prenom' placeholder="Prenom" required><br/>
+  <input type="radio" id="etudiant" name="personne" value="Etudiant">
+  <label for="etudiant">Etudiant</label>
+  <input type="radio" id="enseignant" name="personne" value="Enseignant">
+  <label for="enseignant">Enseignant</label><br>
+  <input type='text' name="mdp" placeholder="Mot de passe" required></br>
+  <button name='inscription_ok' type='submit'>S'inscrire</button>
+</form>
+
+<?php
+if( isset( $_POST["inscription_ok"])){
+        /*Connexion à la bd*/
+        $conn = @mysqli_connect("gpu-epu.univ-savoie.fr:48080", "root", "RootAuPif");
+        mysqli_select_db($conn, "learnagement"); 
+        mysqli_query($conn, "SET NAMES UTF8");
+        echo"azerty";
+?>
