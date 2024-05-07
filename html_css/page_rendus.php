65d91bf9743ccf539994290bc843d87450fbfcda
Emma Rechon-Reguet <emma.rechon-reguet@inria.fr>
2024-05-07 13:40:10 +0200

    Affichage sous forme de puces de la liste des rendus pour enseignants et liste à cocher à faire pour élèves

--- html_css/page_rendus.php
+++ html_css/page_rendus.php
@@ -24,8 +24,8 @@
             mysqli_query($conn, "SET NAMES UTF8");
         }
 
-        /* Afficher la liste des rendus pour un élève*/
-        echo "<h2>Liste des rendus qu'un élève a à rendre </h2> ";
+        /* Afficher la liste des rendus qu'un enseignant a rentré*/
+        echo "<h2>Liste des rendus qu'un enseignant à rentré </h2> ";
         $sql="SELECT m.nom AS nom, date,description FROM `INFO_rendus_eleves` JOIN INFO_module m ON module LIKE m.code_module ORDER BY date ASC";
         $result=mysqli_query($conn, $sql) or die ("Problème lors de la connexion");
 
@@ -35,19 +35,37 @@
         }
         echo "</ul></div>";
 
-        /* Afficher la liste des rendus qu'un enseignants a mis*/
-        echo "<h2>Liste des rendus qu'un enseignants à afficher </h2> ";
-        $sql="SELECT m.nom AS nom, date,description FROM `INFO_rendus_eleves` JOIN INFO_module m ON module LIKE m.code_module ORDER BY date ASC";
+        /* Afficher la liste des rendus qu'un élèves a à faire*/
+        echo "<h2>Liste des rendus qu'un élèves a à faire </h2> ";
+        $sql="SELECT m.nom AS nom, date,description FROM `INFO_rendus` JOIN INFO_module m ON module LIKE m.code_module ORDER BY date ASC";
         $result=mysqli_query($conn, $sql) or die ("Problème lors de la connexion");
 
-        echo "<div id='listerenduseleves'><ul> ";
+        echo "<form> ";
         while ($row=mysqli_fetch_array($result)) {
-            echo"<li>".$row['nom']. " : ". $row['description']. ". Deadline : ".$row['date'] ." ";
+            echo "<input type='checkbox' id='choix' name='choix1' value='choix1'>
+            <label for='choix1'>".$row['nom']. " : ". $row['description']. ". Deadline : ".$row['date'] ."</label><br>";
         }
-        echo "</ul></div>";
+        echo "</form>";
+
 
         /* Permettre à un enseignants de rajouter un rendus*/
+        //echo "<h1> Remplir les éléments </h1>";
+        /* Ajout de la liaison entre actionneur et zone*/
+        /**if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Vérifier si la clé "choix" existe dans $_POST
+            if (isset($_POST["menuactionneur"]) && isset($_POST["menuzones"])) {
+                // Récupération des valeurs sélectionnées dans les listes déroulantes
+                $actionneur = $_POST["menuactionneur"];
+                $zone = $_POST["menuzones"];
 
+                $sql="INSERT INTO actionneur2zone(id_actionneur,id_zone) VALUES ((SELECT id_actionneur FROM actionneur WHERE nom='".$actionneur."'),(SELECT id_zone FROM zone WHERE nom='".$zone ." '))\n";
+                $result=mysqli_query($conn, $sql); // on envoie la requête dans la base de donnée
+                if($result){
+                    $ajoute=true;
+                }
+            }
+        }
         /* Afficher la liste des élèves qui ont rendus un rendus spécifiques*/
 
         /* Permettre à un élève de valider le rendu d'un module*/
+?>
