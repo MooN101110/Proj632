01217272fc1ae96404aee841668dee13ed5e1e02
Louna Camas <lounacamas@gmail.com>
2024-05-14 13:46:45 +0200

    Amélioration code generation mdp

--- admin/generation_mdp.php
+++ admin/generation_mdp.php
@@ -9,6 +9,9 @@
 
 <body>
     <?php
+
+        set_time_limit(300);
+        
         function motDePasse($longueur=8) { // 8 = longueur par défaut
             // chaine de caractères qui sera mis dans le désordre:
             $Chaine = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ&$%-_!?";
@@ -65,10 +68,15 @@
             $resultat = mysqli_query($conn, $sql) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql);
 
 
+            $sql = "INSERT INTO INFO_utilisateur(nom,prenom,identifiant,mot_de_passe,mdp_change) VALUES ";
+            echo $sql;
             while($ligne = mysqli_fetch_array($resultat)){
-                $sql = "INSERT INTO INFO_utilisateur(nom,prenom,identifiant,mot_de_passe,mdp_change) VALUES ('".$ligne['nom']."','".$ligne['prenom']."','".identifiant($ligne['nom'],$ligne['prenom'])."','".password_hash(motDePasse(), PASSWORD_DEFAULT)."',0)";
-                $resultat1 = mysqli_query($conn, $sql) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql);
+                $temp = "('". $ligne['nom']."','".$ligne['prenom']."','".identifiant($ligne['nom'],$ligne['prenom'])."','".password_hash(motDePasse(), PASSWORD_DEFAULT)."',0)";
+                $sql = $sql.$temp.",";
+                echo $temp.",";
             }
+            $sql = substr($sql,0,-1);
+            $resultat1 = mysqli_query($conn, $sql) or die("Requête invalide: ". mysqli_error( $conn )."\n".$sql);
 
         }
             
