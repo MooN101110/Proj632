594e602d21c1e4c51089aa7d98c2e0c91aa8ba90
corailllum <160240585+corailllum@users.noreply.github.com>
2024-05-14 15:57:01 +0200

    Update page_connexion.inc.php

--- html_css/page_connexion.inc.php
+++ html_css/page_connexion.inc.php
@@ -1,9 +1,4 @@
-<h2>Connexion</h2>
-<form method='post' action='?page=verification_mdp'>
-  <input type='text' name='id' placeholder='Identifiant' required><br/>
-  <input type='password' name='mdp' placeholder='Mot de passe' required></br>
-  <button name='inscription_ok' type='submit' value="Popup">Connexion</button>
-</form>
+
 <head>
 
 <link rel="stylesheet" type="text/css" href="connexion.css">
