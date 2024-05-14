f75e14c1105de8d74da715cadbc6baa15416afa5
Axelle <160240449+aquellery@users.noreply.github.com>
2024-05-14 15:32:38 +0200

    Page d'affichage de tous les professeurs et de tous les étudiants

--- html_css/page_liste_personnel.inc.php
+++ html_css/page_liste_personnel.inc.php
@@ -1,17 +1,21 @@
 <?php
-echo "<form action='https://tp-epua.univ-savoie.fr/~royax/proj632/html_css/learnagement.php?page=liste_personnel.php method='get'>";
+
+//Formulaire pour choisir la liste à afficher
+echo "<form method='post' action='?page=liste_personnel'>";
     echo "<label id='formulaire' for='choix'>Selectionnez la liste à afficher : </label>";
     echo "<select name='liste' id='form'>";
-    echo "<option value=liste_prof> Liste des enseignants </option>"; 
-    echo "<option value=liste_elev> Liste des étudiants </option>"; 
+    echo "<option value=liste_prof> Enseignants </option>"; 
+    echo "<option value=liste_etu> Etudiants </option>"; 
     echo "</select>";
-    echo"</br>";
-    echo "<input type='hidden' name='validation' value='true'>"; // Ajout de la variable cachée pour indiquer que le formulaire a été soumis
-    echo "<button id=bouton type='submit' name='validation' >Valider </button><br/>";
-    echo "</form>";
+    echo "<input type='hidden' name='submit' value='true'>"; // Ajout de la variable cachée pour indiquer que le formulaire a été soumis
+    echo "<button id=bouton type='submit'>Valider</button><br/>";
+echo "</form>";
 
 
-
-
+//Affichage de la liste selectionnée 
+if (isset($_POST['submit'])) {
+    $liste=$_POST['liste'];
+    include("page_".$liste.".inc.php");
+}
 
 ?>
