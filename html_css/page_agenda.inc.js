504fbf2b885aa1c52b4d312ce08503c443768fd7
CypGttn <154144790+CypGttn@users.noreply.github.com>
2024-05-21 23:00:20 +0200

    Update page_agenda.inc.js
    Ajout d'événements pour la présentation

--- html_css/page_agenda.inc.js
+++ html_css/page_agenda.inc.js
@@ -4,7 +4,37 @@ window.onload = function() {
         {
             debut: { heure: 8, minutes: 15 },
             fin: { heure: 9, minutes: 45 },
-            jour: 15 // 0 pour lundi
+            jour: 23,
+            module : "ISOC631",
+            backgroundColor : "red"
+        },
+        {
+            debut: { heure: 10, minutes: 15 },
+            fin: { heure: 11, minutes: 45 },
+            jour: 24,
+            module : "INFO631",
+            backgroundColor : "yellow"
+        },
+        {
+            debut: { heure: 8, minutes: 15 },
+            fin: { heure: 11, minutes: 45 },
+            jour: 27,
+            module : "PROJ632",
+            backgroundColor : "green"
+        }, 
+        {
+            debut: { heure: 12, minutes: 15 },
+            fin: { heure: 13, minutes: 30 },
+            jour: 25,
+            module : "Réunion",
+            backgroundColor : "blue"
+        }, 
+        {
+            debut: { heure: 12, minutes: 15 },
+            fin: { heure: 13, minutes: 30 },
+            jour: 26,
+            module : "ISOC631",
+            backgroundColor : "red"
         }
     ];
 
@@ -17,12 +47,12 @@ window.onload = function() {
 
         for (var h = debutHeure; h < finHeure || (h === finHeure && debutMinutes < finMinutes); h++) {
             for (var m = (h === debutHeure ? Math.ceil(debutMinutes / 15) : 0); m < (h === finHeure ? Math.floor(finMinutes / 15) : 4); m++) {
-                var cellId = 'cell_' + ('0' + h).slice(-2) + '_' + ('0' + (m * 15)).slice(-2) + '_' + ('0' + (jour + 1)).slice(-2) + '_' + ('0' + (new Date().getMonth() + 1)).slice(-2) + '_' + new Date().getFullYear();
+                var cellId = 'cell_' + ('0' + h).slice(-2) + '_' + ('0' + (m * 15)).slice(-2) + '_' + ('0' + (jour-1)).slice(-2) + '_' + ('0' + (new Date().getMonth() + 1)).slice(-2) + '_' + new Date().getFullYear();
                 console.log(cellId);
                 var cell = document.getElementById(cellId);
                 if (cell) {
-                    cell.textContent = 'Cours';
-                    cell.style.backgroundColor = "red";
+                    cell.textContent = evenement.module;
+                    cell.style.backgroundColor = evenement.backgroundColor;
                 }
             }
         }
